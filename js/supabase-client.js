6dcb8117d7b71f52746aa716928189be2257ff2c
sharshith241-svg <sharshith241@gmail.com>
2025-11-29 22:16:45 +0530

    fix

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const supabase = createClient(
-  "https://remgafeltdylcbpkpfcd.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlbWdhZmVsdGR5bGNicGtwZmNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMDEyNTMsImV4cCI6MjA3OTU3NzI1M30.TQ0-yWrxSIutkGrnYLES3NcIzAqkzBEeXrxjHnCDFz4"
+  "https://isnbbmzcgpslxtnxmcdh.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzbmJibXpjZ3BzbHh0bnhtY2RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MzI1MTksImV4cCI6MjA4MDAwODUxOX0.6GTURhIphelEUpOEnXFEFkUicNxJgrmHg4HHnvTUyOA"
 );
